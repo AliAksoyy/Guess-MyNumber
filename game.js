e81624e679d7693e90036092ed5e2e9dfd4d06aa
Ali <alifeyza4017@gmail.com>
2022-09-18 06:49:21 +0300

    added

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
 const randomNumber = Math.trunc(Math.random() *100 +1 )
-
+console.log(randomNumber)
 
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value)
-  console.log(tguessInput)
+  const msg = document.querySelector(".msg")
+  if(!guessInput) {
+    msg.textContent = "pleasa a number"
+  }else if (randomNumber === guessInput) {
+    msg.innerText = "Congrats you win"
+    document.querySelector("body").style.background = "green"
+  }
 })
